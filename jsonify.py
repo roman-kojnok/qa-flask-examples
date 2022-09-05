2ab1652eb08b3acb5bcd25a6106a2fedd32eeb0e
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-05 16:31:42 +0100

    Changed dict

--- jsonify.py
+++ jsonify.py
@@ -3,18 +3,16 @@ import json
 
 appFlask = Flask(__name__)
 
-dict = {'username': 'eduCBA', 'account': 'Premium', 'validity': '2709 days'}
-
 
 @appFlask.route('/home_jsonify')
 def home_jsonify():
-    # dict = {'username': 'eduCBA', 'account': 'Premium', 'validity': '2709 days'}
+    dict = {'username': 'eduCBA', 'account': 'Premium', 'validity': '2709 days'}
     return jsonify(dict)
 
 
 @appFlask.route('/home_dumps')
 def home_dumps():
-    # dict = {'username': 'eduCBA', 'account': 'Premium', 'validity': '2709 days'}
+    dict = {'username': 'eduCBA', 'account': 'Premium', 'validity': '2709 days'}
     return json.dumps(dict)
 
 
