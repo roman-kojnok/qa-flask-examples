ddd3710fc6c43890a67ffce1929780dffec3bc09
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-05 16:48:04 +0100

    added GET method

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello world!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
