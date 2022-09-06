ffd6eb391e1415dae9b45ef045b4a51c23525be6
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-06 11:33:58 +0100

    Corrected syntax

--- jsonify.py
+++ jsonify.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/json')
 def json():
-    return jsonify({'key' : 'value', 'listkey': [1, 2, 3, 4, 5, 10]})
+    return jsonify({'key': 'value', 'listkey': [1, 2, 3, 4, 5, 10]})
 
 
 if __name__ == '__main__':
