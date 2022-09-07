f8b22f8e4afa334ad468ea4eb1e831c0ee98b1b6
Roman Kojnok <roman.kojnok@protonmail.com>
2022-09-07 11:20:48 +0100

    Initial commit

---
+++ my_project.py
@@ -0,0 +1,11 @@
+from flask import Flask,  request, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def theform():
+    return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
